feea25fafb35e4f3cfb2435972abc1bad7e72022
Pedro Chagas <pedrochagas.dev@gmail.com>
2026-08-03 21:56:16 -0300

    feat: Adiciona a classe ENUM de cargos e a variavel cargo na classe Usuario

--- src/main/java/com/zpTec/Sistema_Chamados/model/Usuario.java
+++ src/main/java/com/zpTec/Sistema_Chamados/model/Usuario.java
@@ -12,59 +12,87 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="tb_usuario")
-public class Usuario{
+@Table(name = "tb_usuario")
+public class Usuario {
 
-    @Id
-    private Long id;
-    private String nome;
-    private String sobrenome;
-    private String email;
-    private String senha;
+	@Id
+	private Long id;
+	private String nome;
+	private String sobrenome;
+	private String email;
+	private String senha;
+	private Cargo cargo;
 
-    @JsonIgnore
-    @OneToMany(mappedBy = "usuarioCriador", cascade = CascadeType.ALL)
-    private List<Chamado> chamadosCriados;
+	@JsonIgnore
+	@OneToMany(mappedBy = "usuarioCriador", cascade = CascadeType.ALL)
+	private List<Chamado> chamadosCriados;
 
-    @JsonIgnore
-    @OneToMany(mappedBy = "usuarioEncarregado", cascade = CascadeType.ALL)
-    private List<Chamado> ChamadosEncarregados;
+	@JsonIgnore
+	@OneToMany(mappedBy = "usuarioEncarregado", cascade = CascadeType.ALL)
+	private List<Chamado> ChamadosEncarregados;
 
-    public Long getId() {
+	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getSobrenome() {
 		return sobrenome;
 	}
+
 	public void setSobrenome(String sobrenome) {
 		this.sobrenome = sobrenome;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getSenha() {
 		return senha;
 	}
+
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
 
+	public Cargo getCargo() {
+		return cargo;
+	}
 
+	public void setCargo(Cargo cargo) {
+		this.cargo = cargo;
+	}
 
+	public List<Chamado> getChamadosCriados() {
+		return chamadosCriados;
+	}
 
+	public void setChamadosCriados(List<Chamado> chamadosCriados) {
+		this.chamadosCriados = chamadosCriados;
+	}
 
+	public List<Chamado> getChamadosEncarregados() {
+		return ChamadosEncarregados;
+	}
 
+	public void setChamadosEncarregados(List<Chamado> chamadosEncarregados) {
+		ChamadosEncarregados = chamadosEncarregados;
+	}
 
 }
